11bb20961f325bd97a55bb59798f254beed94f20
Anurag Tiwari <52295974+msd7at@users.noreply.github.com>
2020-09-21 18:53:24 +0530

    Updates

---
+++ precache-manifest.9d12862d0a1aacbf6613a48b3665bd0a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a20d67a402d8a521195c9b5a82c612c",
+    "url": "/Reacteasycodesol/index.html"
+  },
+  {
+    "revision": "eeaec8f7cc5cee9584b7",
+    "url": "/Reacteasycodesol/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "79bce64b5e6ff3c27b5c",
+    "url": "/Reacteasycodesol/static/css/main.ffd02d8a.chunk.css"
+  },
+  {
+    "revision": "eeaec8f7cc5cee9584b7",
+    "url": "/Reacteasycodesol/static/js/2.038ac52e.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/Reacteasycodesol/static/js/2.038ac52e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79bce64b5e6ff3c27b5c",
+    "url": "/Reacteasycodesol/static/js/main.c233d9bf.chunk.js"
+  },
+  {
+    "revision": "9d08c5af5f8a3e22bb5e",
+    "url": "/Reacteasycodesol/static/js/runtime-main.33f53cee.js"
+  },
+  {
+    "revision": "49318c1ed80063864e222ec13af59a30",
+    "url": "/Reacteasycodesol/static/media/and.49318c1e.jpg"
+  },
+  {
+    "revision": "3510798b6ed2578ea22048b9754bf766",
+    "url": "/Reacteasycodesol/static/media/apd.3510798b.jpg"
+  },
+  {
+    "revision": "1cca0901c9914fea43181b4a813bcef2",
+    "url": "/Reacteasycodesol/static/media/digi.1cca0901.jpg"
+  },
+  {
+    "revision": "71dc9a468f03838251e0470fac906b6c",
+    "url": "/Reacteasycodesol/static/media/dm.71dc9a46.jpg"
+  },
+  {
+    "revision": "bcc3a78a0509639fae80949e477dcd16",
+    "url": "/Reacteasycodesol/static/media/img2.bcc3a78a.jpg"
+  },
+  {
+    "revision": "a3c9201368b56a091dee151f38365020",
+    "url": "/Reacteasycodesol/static/media/sw.a3c92013.jpg"
+  }
+]);
